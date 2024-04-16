130b5b85099b0d95a17e5b2afa91d6e4b35cc2d4
root <root@Asus-ROG>
2024-04-16 21:30:37 +0900

    [ADD]StackAndQueue Q6

--- Stack_and_Queue/Q6_C_SQ.c
+++ Stack_and_Queue/Q6_C_SQ.c
@@ -111,7 +111,10 @@ int main()
 
 void removeUntil(Stack *s, int value)
 {
-/* add your code here */
+	while (s->ll.head->item != value)
+	{
+		pop(s);
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////////////
